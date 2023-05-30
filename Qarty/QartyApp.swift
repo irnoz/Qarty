1b525df91753f9a692da82653793c376975b9a4d
irakli nozadze <iraklinozadze2509@gmail.com>
2023-05-30 15:22:04 +0400

    Initial Commit

---
+++ Qarty/QartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QartyApp.swift
+//  Qarty
+//
+//  Created by Irakli Nozadze on 30.05.23.
+//
+
+import SwiftUI
+
+@main
+struct QartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
